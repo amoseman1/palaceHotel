27edffc821445c1e38f8a74e016b3aa764035621
Amy Moseman <amoseman18@gmail.com>
2021-08-30 16:02:03 -0600

    playing with getting hero image to work, added logo to favicon

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import Homepage from './pages/Home/Homepage';
 
 function App() {
   return (
+
     <Router>
+      <link rel="shortcut icon" href="/palacefavicon.png" />
       <div className="App">
         <Route exact path="/" component={Homepage} />
       </div>
--- client/src/pages/Home/Homepage.js
+++ client/src/pages/Home/Homepage.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import "./Homepage.css"
 
 export default function Homepage() {
     return (
-        <div>Hello everyone!</div>
+        <>
+            {/* <div>Hello everyone!</div> */}
+            {/* <img id="photo" src="./palace1.png" alt="birdseye view"></img> */}
+
+
+            <div class="hero-image">
+                <div class="hero-text">
+                    <h1>I am John Doe</h1>
+                    <p>And I'm a Photographer</p>
+                    <button>Hire me</button>
+                </div>
+            </div>
+        </>
     )
 }
 
